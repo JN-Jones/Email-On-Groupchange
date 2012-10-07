4c3e09a1ff3939f62947b84e35b5af4b7a8332fb
Jones <admin@jonesboard.tk>
2012-10-07 20:13:19 +0200

    Initial Release

---
+++ admin/modules/user/eog.php
@@ -0,0 +1,212 @@
+<?php
+if(!defined("IN_MYBB"))
+{
+	header("HTTP/1.0 404 Not Found");
+	exit;
+}
+
+if(function_exists("myplugins_info"))
+    define(MODULE, "myplugins-eog");
+else
+    define(MODULE, "user-eog");
+
+$page->add_breadcrumb_item("Email on Groupchange", "index.php?module=".MODULE);
+
+if($mybb->input['action'] == "do_add") {
+	if(!verify_post_check($mybb->input['my_post_key']))
+	{
+		flash_message("Da lief wohl was falsch... Bitte versuche es erneut", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+
+    if(!strlen(trim($mybb->input['gid'])))
+	{
+		flash_message("Du hast keine Gruppe ausgewählt", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+	if(!strlen(trim($mybb->input['subject'])))
+	{
+		flash_message("Du hast keinen Titel angegeben", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+	if(!strlen(trim($mybb->input['message'])))
+	{
+		flash_message("Du hast keine Nachricht angegeben", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+	
+	$insert = array(
+		"gid" => (int) $mybb->input['gid'],
+		"subject" => $db->escape_string($mybb->input['subject']),
+		"message" => $db->escape_string($mybb->input['message'])
+	);
+	$db->insert_query("eog", $insert);
+
+	flash_message("Erfolgreich hinzugefügt", 'success');
+	admin_redirect("index.php?module=".MODULE);
+
+} elseif($mybb->input['action'] == "add") {
+	$page->add_breadcrumb_item("Hinzufügen", "index.php?module=".MODULE."&amp;action=add");
+	$page->output_header("Hinzufügen");
+	generate_tabs("add");
+	
+	$form = new Form("index.php?module=".MODULE."&amp;action=do_add", "post");
+	$form_container = new FormContainer("Hinzufügen");
+
+	$add_group = $form->generate_group_select("gid");
+	$form_container->output_row("Gruppe", "Wähle die Gruppe bei der diese Email geschickt werden soll", $add_group);
+
+	$add_subject = $form->generate_text_box("subject");
+	$form_container->output_row("Titel", "Hier kannst du den Titel der Email festlegen", $add_subject);
+
+	$add_message = $form->generate_text_area("message");
+	$form_container->output_row("Nachricht", "Hier kannst du die Nachricht der Email festlegen", $add_message);
+
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button("Speichern");
+	$buttons[] = $form->generate_reset_button($lang->reset);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+
+} elseif($mybb->input['action']=="delete") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message("Du hast keine Email zum Löschen ausgewählt", 'error');
+		admin_redirect("index.php?module=".MODULE);
+	}
+	$id=intval($mybb->input['id']);
+	$db->delete_query("eog", "id='{$id}'");
+	flash_message("Email erfolgreich gelöscht", 'success');
+	admin_redirect("index.php?module=".MODULE);
+
+} elseif($mybb->input['action']=="do_edit") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message("Du hast keine Email zum Bearbeiten ausgewählt", 'error');
+		admin_redirect("index.php?module=".MODULE);
+	}
+	$id=intval($mybb->input['id']);
+	if(!verify_post_check($mybb->input['my_post_key']))
+	{
+		flash_message("Da lief wohl was falsch... Bitte versuche es erneut", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=edit&id=$id");
+	}
+
+    if(!strlen(trim($mybb->input['gid'])))
+	{
+		flash_message("Du hast keine Gruppe ausgewählt", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+	if(!strlen(trim($mybb->input['subject'])))
+	{
+		flash_message("Du hast keinen Titel angegeben", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+	if(!strlen(trim($mybb->input['message'])))
+	{
+		flash_message("Du hast keine Nachricht angegeben", 'error');
+		admin_redirect("index.php?module=".MODULE."&action=add");
+	}
+
+
+	$update = array(
+		"gid" => (int) $mybb->input['gid'],
+		"subject" => $db->escape_string($mybb->input['subject']),
+		"message" => $db->escape_string($mybb->input['message'])
+	);
+	$db->update_query("eog", $update, "id='{$id}'");
+	flash_message("Email erfolgreich bearbeitet", 'success');
+	admin_redirect("index.php?module=".MODULE);
+
+} elseif($mybb->input['action']=="edit") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message("Du hast keine Email zum Bearbeiten ausgewählt", 'error');
+		admin_redirect("index.php?module=".MODULE);
+	}
+	$id=intval($mybb->input['id']);
+	$query = $db->simple_select("eog", "*", "id='{$id}'");
+	if($db->num_rows($query) != 1)
+	{
+		flash_message("Interner Fehler beim Auslesen der bisherigen Email", 'error');
+		admin_redirect("index.php?module=".MODULE);
+	}
+	$eog = $db->fetch_array($query);
+
+	$page->add_breadcrumb_item("Bearbeiten", "index.php?module=".MODULE."&amp;action=edit&amp;id=$id");
+	$page->output_header("Email");
+	generate_tabs("list");
+
+	$form = new Form("index.php?module=".MODULE."&amp;action=do_edit", "post");
+	$form_container = new FormContainer("Email");
+
+	$add_group = $form->generate_group_select("gid", array($eog['gid']));
+	$form_container->output_row("Gruppe", "Wähle die Gruppe bei der diese Email geschickt werden soll", $add_group);
+
+	$add_subject = $form->generate_text_box("subject", $eog['subject']);
+	$form_container->output_row("Titel", "Hier kannst du den Titel der Email festlegen", $add_subject);
+
+	$add_message = $form->generate_text_area("message", $eog['message']);
+	$form_container->output_row("Nachricht", "Hier kannst du die Nachricht der Email festlegen", $add_message);
+
+	echo $form->generate_hidden_field("id", $id);
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button("Speichern");
+	$buttons[] = $form->generate_reset_button($lang->reset);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+
+} else {
+	$page->output_header("Emails");
+	generate_tabs("list");
+
+	$table = new Table;
+	$table->construct_header("Gruppe");
+	$table->construct_header("Titel");
+	$table->construct_header("Email");
+	$table->construct_header("Optionen", array("colspan"=>"2"));
+
+	$query = $db->simple_select("eog", "*", "", array("order_by"=>"gid"));
+	if($db->num_rows($query) > 0)
+	{
+		while($eog = $db->fetch_array($query))
+		{
+			$table->construct_cell($groupscache[$eog['gid']]['title']);
+			$table->construct_cell($eog['subject']);
+			if(strlen($eog['message']) > 20)
+			    $eog['message'] = substr($eog['message'], 0, 17)." [...]";
+			$table->construct_cell($eog['message']);
+			$table->construct_cell("<a href=\"index.php?module=".MODULE."&amp;action=edit&amp;id={$eog['id']}\">{$lang->edit}</a>", array('class' => 'align_center', 'width' => '10%'));
+			$table->construct_cell("<a href=\"index.php?module=".MODULE."&amp;action=delete&amp;id={$eog['id']}\">{$lang->delete}</a>", array('class' => 'align_center', 'width' => '10%'));
+			$table->construct_row();
+		}
+	} else {
+		$table->construct_cell("Bisher sind keine Emails hinterlegt", array('class' => 'align_center', 'colspan' => 5));
+		$table->construct_row();
+	}
+	$table->output("Emails");
+}
+
+$page->output_footer();
+
+function generate_tabs($selected)
+{
+	global $page;
+
+	$sub_tabs = array();
+	$sub_tabs['list'] = array(
+		'title' => "Emails auflisten",
+		'link' => "index.php?module=".MODULE,
+		'description' => "Hier sind alle Emails aufgelistet"
+	);
+	$sub_tabs['add'] = array(
+		'title' => "Email hinzufügen",
+		'link' => "index.php?module=".MODULE."&amp;action=add",
+		'description' => "Hier kannst du eine Email hinzufügen"
+	);
+
+	$page->output_nav_tabs($sub_tabs, $selected);
+}
+?>
---
+++ inc/plugins/eog.php
@@ -0,0 +1,144 @@
+<?php
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+if(!$pluginlist)
+    $pluginlist = $cache->read("plugins");
+
+$plugins->add_hook("usercp_usergroups_join_group", "eog_group_join");
+$plugins->add_hook("managegroup_do_joinrequests_start", "eog_group_joinrequest");
+$plugins->add_hook("admin_user_users_edit_commit", "eog_admin");
+
+if(is_array($pluginlist['active']) && in_array("myplugins", $pluginlist['active'])) {
+	$plugins->add_hook("myplugins_actions", "eog_myplugins_actions");
+	$plugins->add_hook("myplugins_permission", "eog_admin_user_permissions");
+} else {
+	$plugins->add_hook("admin_user_menu", "eog_admin_user_menu");
+	$plugins->add_hook("admin_user_action_handler", "eog_admin_user_action_handler");
+	$plugins->add_hook("admin_user_permissions", "eog_admin_user_permissions");
+}
+
+function eog_info()
+{
+	return array(
+		"name"			=> "Email on Groupchange",
+		"description"	=> "Schreibt eine Email an Nutzer welche einer neuen Gruppe beitreten",
+		"website"		=> "http://jonesboard.tk/",
+		"author"		=> "Jones",
+		"authorsite"	=> "http://jonesboard.tk",
+		"version"		=> "1.0",
+		"guid" 			=> "",
+		"compatibility" => "*"
+	);
+}
+
+function eog_activate()
+{
+	global $db;
+
+	$db->query("CREATE TABLE `".TABLE_PREFIX."eog` (
+				`id` int(11) NOT NULL AUTO_INCREMENT,
+				`gid` int(10),
+				`subject` varchar(100) NOT NULL,
+				`message` text NOT NULL,
+	PRIMARY KEY (`id`) ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1");
+}
+
+function eog_deactivate()
+{
+    global $db;
+
+	$db->drop_table("eog");
+}
+
+function eog_myplugins_actions($actions)
+{
+	global $page, $info;
+
+	$actions['eog'] = array(
+		"active" => "eog",
+		"file" => "../user/eog.php"
+	);
+
+	$sub_menu = array();
+	$sub_menu['10'] = array("id" => "eog", "title" => "Email on Groupchange", "link" => "index.php?module=myplugins-eog");
+
+	$sidebar = new SidebarItem("Email on Groupchange");
+	$sidebar->add_menu_items($sub_menu, $actions[$info]['active']);
+
+	$page->sidebar .= $sidebar->get_markup();
+
+	return $actions;
+}
+
+function eog_admin_user_menu($sub_menu)
+{
+	$sub_menu[] = array("id" => "eog", "title" => "Email on Groupchange", "link" => "index.php?module=user-eog");
+
+	return $sub_menu;
+}
+
+function eog_admin_user_action_handler($actions)
+{
+	$actions['eog'] = array(
+		"active" => "eog",
+		"file" => "eog.php"
+	);
+
+	return $actions;
+}
+
+function eog_admin_user_permissions($admin_permissions)
+{
+	$admin_permissions['eog'] = "Kann die Email bei Gruppenwechsel ändern";
+
+	return $admin_permissions;
+}
+
+function eog_group_join()
+{
+	global $mybb, $usergroup;
+	
+	eog_email($mybb->user, $usergroup);
+}
+
+function eog_group_joinrequest()
+{
+	global $mybb, $groupscache, $gid;
+
+	if(is_array($mybb->input['request'])) {
+		foreach($mybb->input['request'] as $uid => $what) {
+			if($what == "accept") {
+				$user = get_user($uid);
+				eog_email($user, $groupscache[$gid]);
+			}
+		}
+	}
+}
+
+function eog_admin()
+{
+	global $groupscache, $user, $updated_user;
+
+	if($user['usergroup'] != $updated_user['usergroup'])
+		eog_email($updated_user, $groupscache[$updated_user['usergroup']]);
+}
+
+function eog_email($user, $group)
+{
+	global $db;
+	
+	$query = $db->simple_select("eog", "*", "gid='{$group['gid']}'");
+	if($db->num_rows($query) == 0)
+	    return;
+	while($eog = $db->fetch_array($query)) {
+		$subject = str_replace("{user}", $user['username'], $eog['subject']);
+		$subject = str_replace("{group}", $group['title'], $subject);
+		$message = str_replace("{user}", $user['username'], $eog['message']);
+		$message = str_replace("{group}", $group['title'], $message);
+
+		my_mail($user['email'], $subject, $message);
+	}
+}
+?>
